d6eaf6974d9345c36dca8d7ee814f7dfcf9cb1a7
bahadirmemis <sadikbahadir.memis@migrosonline.com>
2024-02-25 11:19:53 +0300

    Refactor

--- src/main/java/com/bahadirmemis/n11mailsender/controller/MailController.java
+++ src/main/java/com/bahadirmemis/n11mailsender/controller/MailController.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -20,7 +21,7 @@ public class MailController {
   }
 
   @GetMapping("{id}/infos")
-  public MailInfoDTO getMailSendInfoDTO(@PathVariable Long id) {
-    return new MailInfoDTO("yusuf@memis.com", "sbahadirm@gmail.com", LocalDateTime.now(), "Naber oğlum!");
+  public MailInfoDTO getMailSendInfoDTO(@PathVariable Long id, @RequestParam String topic) {
+    return new MailInfoDTO("yusuf@memis.com", "sbahadirm@gmail.com", LocalDateTime.now(), topic);
   }
 }
