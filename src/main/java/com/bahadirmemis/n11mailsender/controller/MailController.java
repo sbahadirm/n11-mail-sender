72d216929602a1686dea7b33c41d953c1ddb2cdc
bahadirmemis <sadikbahadir.memis@migrosonline.com>
2024-02-25 11:31:42 +0300

    Add sendMailPut api

--- src/main/java/com/bahadirmemis/n11mailsender/controller/MailController.java
+++ src/main/java/com/bahadirmemis/n11mailsender/controller/MailController.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -37,5 +38,12 @@ public class MailController {
     log.info("Body: " + request.mailBody());
 
     return new MailInfoDTO(request.receiver(), getDefaultMailAddress(), LocalDateTime.now(), request.topic());
+  }
+
+  @PutMapping
+  public void sendMailPut(@RequestBody SendMailRequest request) {
+
+    log.info("Mail send to " + request.receiver());
+    log.info("Body: " + request.mailBody());
   }
 }
